ee8d21fd1a8400905c8a270938952356b7f6f32a
learngithubz <perjalananswift@gmail.com>
2024-07-28 11:24:57 +0700

    seccond commit

--- SwiftSourceControl/ContentView.swift
+++ SwiftSourceControl/ContentView.swift
@@ -14,6 +14,13 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Putut Yusri Bahtiar!")
+            
+            Button {
+                <#code#>
+            } label: {
+                Text("Click me")
+            }
+
         }
         .padding()
     }
