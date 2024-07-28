c91d94f592961aba7f91340cadcf6a3b6fb0fa0f
learngithubz <perjalananswift@gmail.com>
2024-07-28 11:58:19 +0700

    [Clean] Add description for commit message rule.

--- SwiftSourceControl/ContentView.swift
+++ SwiftSourceControl/ContentView.swift
@@ -5,6 +5,24 @@
 //  Created by Putut Yusri Bahtiar on 28/07/24.
 //
 
+/*
+ COMMIT MESSAGE RULE:
+ NEW FEATURE :
+ [Feature] Description of the feature you add
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of the patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MUNDANE TASK:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release.
+ */
+
 import SwiftUI
 
 struct ContentView: View {
