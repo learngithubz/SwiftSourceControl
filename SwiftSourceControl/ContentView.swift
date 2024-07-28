91edeb89f68c1c3899a70ecae8f2525d1fcbf0c2
pututyb <pututyb@gmail.com>
2024-07-28 10:18:54 +0700

    Initial Commit

---
+++ SwiftSourceControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftSourceControl
+//
+//  Created by Putut Yusri Bahtiar on 28/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftSourceControl/SwiftSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSourceControlApp.swift
+//  SwiftSourceControl
+//
+//  Created by Putut Yusri Bahtiar on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
