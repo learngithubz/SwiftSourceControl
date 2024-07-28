910d826d726fe0663fc3ac3cae73a58dd477e5fb
learngithubz <perjalananswift@gmail.com>
2024-07-28 11:33:01 +0700

    Add HomeVieww

---
+++ SwiftSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftSourceControl
+//
+//  Created by Putut Yusri Bahtiar on 28/07/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+#Preview {
+    HomeView()
+}
